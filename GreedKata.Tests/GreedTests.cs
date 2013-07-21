9f7bc3f7cefd6972bedac4b3ad97c24b69e9e289
Dennis Somerville <dennis.somerville@gmail.com>
2013-07-21 11:00:14 -0400

    Added test cases from instructions.

--- GreedKata.Tests/GreedTests.cs
+++ GreedKata.Tests/GreedTests.cs
@@ -29,11 +29,24 @@ namespace GreedKata.Tests
         }
 
         [Test]
-        [TestCase(1,500)]
-        [TestCase(5,250)]
+        [TestCase(1, 500)]
+        [TestCase(5, 250)]
         public void CalculateScore_AllSameNumber(int numberToTest, int exptectedScore)
         {
             TestRoll(numberToTest.CreateArrayOf(5), exptectedScore);
         }
+
+        [Test]
+        [TestCase(new[] { 1, 1, 1, 5, 1 }, 1150, Ignore = true)]
+        [TestCase(new[] { 2, 3, 4, 6, 2 }, 0)]
+        [TestCase(new[] { 3, 4, 5, 3, 3 }, 350, Ignore = true)]
+        [TestCase(new[] { 1, 5, 1, 2, 4 }, 250)]
+        [TestCase(new[] { 5, 5, 5, 5, 5 }, 600, Ignore = true)]
+        public void CalculateScore_ExamplesFromInstructions(int[] numbersToTest, int expectedScore)
+        {
+            TestRoll(numbersToTest, expectedScore);
+        }
     }
+
+
 }
