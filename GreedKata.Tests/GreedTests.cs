261246cd1cd32706da1d8064957f06c36b40caf7
Dennis Somerville <dennis.somerville@gmail.com>
2013-07-21 10:49:45 -0400

    refactored

--- GreedKata.Tests/GreedTests.cs
+++ GreedKata.Tests/GreedTests.cs
@@ -1,21 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace GreedKata.Tests
 {
     [TestFixture]
     public class GreedTests
     {
+        #region Helper Methods
         private static void TestRoll(int[] roll, int expectedScore)
         {
             var score = Greed.CalculateScore(roll);
 
             Assert.That(score, Is.EqualTo(expectedScore));
         }
+        #endregion
+
 
         [Test]
         [TestCase(1, 100)]
@@ -31,9 +29,11 @@ namespace GreedKata.Tests
         }
 
         [Test]
-        public void CalculateScore_AllTwos()
+        [TestCase(1,500)]
+        [TestCase(5,250)]
+        public void CalculateScore_AllSameNumber(int numberToTest, int exptectedScore)
         {
-            TestRoll(2.CreateArrayOf(5), 0);
+            TestRoll(numberToTest.CreateArrayOf(5), exptectedScore);
         }
     }
 }
